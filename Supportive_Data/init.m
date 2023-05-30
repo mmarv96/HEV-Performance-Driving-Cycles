062ee1448a85085c2444b06c56f2c226bc9f179e
mmarv96 <58235749+mmarv96@users.noreply.github.com>
2023-05-30 12:36:30 -0400

    change some files

--- Supportive_Data/init.m
+++ Supportive_Data/init.m
@@ -1,30 +1,30 @@
 
 clear
-M_glider = 1746;  % glider mass, kg
-M_driver = 70; % driver mass kg
-C_D = 0.35;    % drag coefficient
-C_0 = 0.015;   % rolling resistance coefficient
-A_F = 1.93;    % frontal area, m^2
-eta_ess = 0.8; % energy storage subsystem round-trip efficiency
-r_wheel = 0.2794; % wheel radius, m
-P_eng_min = 10000.0; % min engine power, W
-P_eng_max = 85000.0; % max engine power, W
-G_diff = 0.268;     % differential gear ratio
-G_cvt_min = 0.5;  % minimum tansmission gear ratio
-w_eng_min = 1000; % minimum engine speed, rpm
-E_batt = 2.0;   % battery capacity, kW
-SOC_init = 0.5; % initial SOC
-m_batt = 21.74; % battery mass, kg/kw-hr
-G_elec = 1; % gear ratio for motor
+M_glider = 1746;        % glider mass, kg
+M_driver = 70;          % driver mass kg
+C_D = 0.35;             % drag coefficient
+C_0 = 0.015;            % rolling resistance coefficient
+A_F = 1.93;             % frontal area, m^2
+eta_ess = 0.8;          % energy storage subsystem round-trip efficiency
+r_wheel = 0.2794;       % wheel radius, m
+P_eng_min = 10000.0;    % min engine power, W
+P_eng_max = 85000.0;    % max engine power, W
+G_diff = 0.268;         % differential gear ratio
+G_cvt_min = 0.5;        % minimum tansmission gear ratio
+w_eng_min = 1000;       % minimum engine speed, rpm
+E_batt = 2.0;           % battery capacity, kW
+SOC_init = 0.5;         % initial SOC
+m_batt = 21.74;         % battery mass, kg/kw-hr
+G_elec = 1;             % gear ratio for motor
 
 % physical constants
-rho = 1.225; % density of air, kg/m^3
-g = 9.8; % acceleration due to gravity,  m/s^s
+rho = 1.225;            % density of air, kg/m^3
+g = 9.8;                % acceleration due to gravity,  m/s^s
 
 % unit conversions
-m_to_mi = 1/1609; % meters to miles
-g_per_hr_to_gal_per_s = 9.778e-8;  % g/hr to gal/s
-mi_per_hr_to_m_per_s = 0.44704; % mi/hr to m/s
+m_to_mi = 1/1609;       % meters to miles
+g_per_hr_to_gal_per_s = 9.778e-8;       % g/hr to gal/s
+mi_per_hr_to_m_per_s = 0.44704;         % mi/hr to m/s
 
 % calculated constants
 
